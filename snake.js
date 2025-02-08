50827d1cc4e2559396a7d6cd1fbfb087a9e71cd7
MaksPzm <makspzm@gmail.com>
2025-02-08 23:39:57 +0500

    добавлено управление

--- snake.js
+++ snake.js
@@ -78,3 +78,98 @@ showFood.showRadom()                                                        //в
 if (JSON.stringify(showSnake.coordinates) == JSON.stringify(showFood)) {
     showFood.showRadom()
 }
+
+function assClass() {
+    snakeBody[0].classList.add('snakeBody');
+    for (let i = 0; i < snakeBody.length; i++) {
+        snakeBody[i].classList.add('snakeBody');
+    }
+}
+
+
+let direction = "ArrowUp"; // если эту переменную создаем в функции события, тогда она при каждом нажатии перезаписывается заново
+    
+// управление на стрелках    
+window.addEventListener('keydown', function(e) {
+console.log('direction: ', direction);
+if (e.key == "ArrowUp" && (direction != 'ArrowDown')) {
+    console.log('e.key : ', e.key );
+    direction = 'ArrowUp';
+    console.log('true: ', true);
+} else if (e.key == "ArrowDown" && (direction != 'ArrowUp')) {
+    console.log('e.key: ', e.key);
+    true
+    console.log('true: ', true);
+    direction = 'ArrowDown';
+} else if (e.key == "ArrowLeft" && direction != ('ArrowRight')) {
+    console.log('e.key: ', e.key);
+    true
+    console.log('true: ', true);
+    direction = 'ArrowLeft';
+} else if (e.key == "ArrowRight"  && direction != ('ArrowLeft')) {
+    console.log('e.key: ', e.key);
+    true
+    console.log('true: ', true);
+    direction = 'ArrowRight';
+}
+})
+    
+    
+function move() {      
+    let snakeBody = Array.from(document.querySelectorAll('.snakeBody'));
+    const coordinatesSnake = [snakeBody[0].getAttribute('data-X'), snakeBody[0].getAttribute('data-Y')];
+    console.log('direction: ', direction);
+    // direction.splice(0, 2); // удаляем из массива лишние элементы при каждом нажатии
+
+    console.log('coordinatesSnake: ', coordinatesSnake);
+            
+    snakeBody[0].classList.toggle('snakeBody');
+    // snakeBody[snakeBody.length - 1].remove('snakeBody');
+    snakeBody.pop();
+                
+                
+    if (direction == 'ArrowUp') {
+        
+        if (coordinatesSnake[1] < 10) {
+            snakeBody.unshift(document.querySelector('[data-X = "' + coordinatesSnake[0] + '"][data-Y = "' + (+coordinatesSnake[1] + 1) + '"]'));
+            console.log('snakeBody2: ', snakeBody);  
+        } else {
+            snakeBody.unshift(document.querySelector('[data-X = "' + coordinatesSnake[0] + '"][data-Y = "1"]'));
+        }   
+    } 
+    if (direction == 'ArrowDown') {
+        
+        if (coordinatesSnake[1] > 1) {
+            snakeBody.unshift(document.querySelector('[data-X = "' + coordinatesSnake[0] + '"][data-Y = "' + (+coordinatesSnake[1] - 1) + '"]'));
+            console.log('snakeBody2: ', snakeBody);  
+        } else {
+            snakeBody.unshift(document.querySelector('[data-X = "' + coordinatesSnake[0] + '"][data-Y = "10"]'));
+        }
+    }
+    if (direction == 'ArrowLeft') {
+        
+        if (coordinatesSnake[0] > 1) {
+            snakeBody.unshift(document.querySelector('[data-X = "' + (+coordinatesSnake[0] - 1) + '"][data-Y = "' + coordinatesSnake[1] + '"]'));
+            console.log('snakeBody2: ', snakeBody);  
+        } else {
+            snakeBody.unshift(document.querySelector('[data-X = "10"][data-Y = "' + coordinatesSnake[1] + '"]'));
+        } 
+    } 
+    if (direction == 'ArrowRight') {
+        
+        if (coordinatesSnake[0] < 10) {
+            snakeBody.unshift(document.querySelector('[data-X = "' + (+coordinatesSnake[0] + 1) + '"][data-Y = "' + coordinatesSnake[1] + '"]'));
+            console.log('snakeBody2: ', snakeBody);  
+        } else {
+            snakeBody.unshift(document.querySelector('[data-X = "1"][data-Y = "' + coordinatesSnake[1] + '"]'));
+        }
+    }
+
+    snakeBody[0].classList.add('snakeBody');
+    for (let i = 0; i < snakeBody.length; i++) {
+    snakeBody[i].classList.add('snakeBody');
+    }
+}
+
+
+let interval = setInterval(move, 300)
