6892020f859995a038152349f5cface185a4d516
MaksPzm <makspzm@gmail.com>
2025-02-09 21:40:12 +0500

    добавленно увеличение змеи при съедании еду и конец игре...

--- snake.js
+++ snake.js
@@ -46,7 +46,7 @@ const min = 1, max = 10;
 //выводим рандомно змею
 function showRandomSnake() {
 let randomPositionX = Random(max, min);
-let randomPositionY = Random(max,min);
+let randomPositionY = Random(max, 2);
 return [randomPositionX, randomPositionY]
 }
 //console.log('showRandomSnake(): ', showRandomSnake());
@@ -56,75 +56,69 @@ class Coordinate {
         this.coordinates = coordinates;
         this.className = className;
     }
-    showRadom() {
+    showRadomSnake() {
+        let randomeKube = [document.querySelector(`[data-X = "${this.coordinates[0]}"][data-Y = "${this.coordinates[1]}"]`), document.querySelector(`[data-X = "${this.coordinates[0]}"][data-Y = "${this.coordinates[1] - 1}"]`)];
+        //console.log('randomeKube: ', randomeKube);
+        for (let i = 0; i < randomeKube.length; i++){
+            randomeKube[i].classList.add(this.className);
+        }
+        randomeKube[0].classList.add('snakeHead');
+    }
+    showRadomFood() {
         let randomeKube = [document.querySelector(`[data-X = "${this.coordinates[0]}"][data-Y = "${this.coordinates[1]}"]`)];
         //console.log('randomeKube: ', randomeKube);
         for (let i = 0; i < randomeKube.length; i++){
             randomeKube[i].classList.add(this.className);
         }
-        randomeKube[0].classList.add(this.className);
+        randomeKube[0].classList.add('snakeHead');
     }
 }
 
 const showSnake = new Coordinate(showRandomSnake(), 'snakeBody'); 
 //console.log('showSnake: ', showSnake.coordinates);
-showSnake.showRadom()                                                       //выводим на экран змею
+showSnake.showRadomSnake()                                                       //выводим на экран змею
+// console.log('showSnake.showRadomSnake()  : ', showSnake.showRadomSnake()  );
 
-const showFood = new Coordinate(showRandomSnake(), 'food');
-//console.log('showFood: ', showFood.coordinates);
-showFood.showRadom()                                                        //выводим на экран еду
 
-//сравниваем массивы, если координаты совпадают, еще раз перезапускаем еду
-if (JSON.stringify(showSnake.coordinates) == JSON.stringify(showFood)) {
-    showFood.showRadom()
-}
 
 function assClass() {
-    snakeBody[0].classList.add('snakeBody');
-    for (let i = 0; i < snakeBody.length; i++) {
+    snakeBody[0].classList.add('snakeHead');
+    for (let i = 0; i < snakeBody.length - 1; i++) {
         snakeBody[i].classList.add('snakeBody');
     }
 }
 
+const showFood = new Coordinate(showRandomSnake(), 'food');
+//console.log('showFood: ', showFood.coordinates);
+showFood.showRadomFood()                                                        //выводим на экран еду
+
+
+let snakeBody = Array.from(document.querySelectorAll('.snakeBody'));
+let snakeHead = document.querySelector('.snakeHead'); 
+let food = document.querySelector('.food');
+
+//сравниваваем координаты классов еды и змеи, если координаты совпадают, еще раз перезапускаем еду
+while (food.classList.contains('snakeBody')) {
+    showFood.showRadomFood()
+}
 
 let direction = "ArrowUp"; // если эту переменную создаем в функции события, тогда она при каждом нажатии перезаписывается заново
     
 // управление на стрелках    
-window.addEventListener('keydown', function(e) {
-console.log('direction: ', direction);
-if (e.key == "ArrowUp" && (direction != 'ArrowDown')) {
-    // console.log('e.key : ', e.key );
-    direction = 'ArrowUp';
-    console.log('true: ', true);
-} else if (e.key == "ArrowDown" && (direction != 'ArrowUp')) {
-    console.log('e.key: ', e.key);
-    true
-    // console.log('true: ', true);
-    direction = 'ArrowDown';
-} else if (e.key == "ArrowLeft" && direction != ('ArrowRight')) {
-    console.log('e.key: ', e.key);
-    true
-    // console.log('true: ', true);
-    direction = 'ArrowLeft';
-} else if (e.key == "ArrowRight"  && direction != ('ArrowLeft')) {
-    console.log('e.key: ', e.key);
-    true
-    // console.log('true: ', true);
-    direction = 'ArrowRight';
-}
-})
-    
-let snakeBody = Array.from(document.querySelectorAll('.snakeBody')); 
 
-function move() {      
+  let stop = false;  
+
+
+console.log(snakeBody);
+function move() {    
     const coordinatesSnake = [snakeBody[0].getAttribute('data-X'), snakeBody[0].getAttribute('data-Y')];   
     // console.log('direction: ', direction);
     // direction.splice(0, 2); // удаляем из массива лишние элементы при каждом нажатии
-
-    console.log('coordinatesSnake: ', coordinatesSnake);
-            
-    snakeBody[0].classList.toggle('snakeBody');
-    // snakeBody[snakeBody.length - 1].remove('snakeBody');
+    
+    // console.log('coordinatesSnake: ', coordinatesSnake);
+    
+    snakeBody[0].classList.remove('snakeHead');
+    snakeBody[snakeBody.length-1].classList.remove('snakeBody');  
     snakeBody.pop();
                 
                 
@@ -136,6 +130,7 @@ function move() {
         } else {
             snakeBody.unshift(document.querySelector('[data-X = "' + coordinatesSnake[0] + '"][data-Y = "1"]'));
         }   
+        
     } 
     if (direction == 'ArrowDown') {
         
@@ -145,6 +140,8 @@ function move() {
         } else {
             snakeBody.unshift(document.querySelector('[data-X = "' + coordinatesSnake[0] + '"][data-Y = "10"]'));
         }
+            
+            
     }
     if (direction == 'ArrowLeft') {
         
@@ -154,6 +151,7 @@ function move() {
         } else {
             snakeBody.unshift(document.querySelector('[data-X = "10"][data-Y = "' + coordinatesSnake[1] + '"]'));
         } 
+       
     } 
     if (direction == 'ArrowRight') {
         
@@ -163,51 +161,102 @@ function move() {
         } else {
             snakeBody.unshift(document.querySelector('[data-X = "1"][data-Y = "' + coordinatesSnake[1] + '"]'));
         }
+        
     }
 
-    snakeBody[0].classList.add('snakeBody');
+    //устанавливаем, что должно произойти если змея коснется саму себя
+    if (snakeBody[0].classList.contains('snakeBody')) {
+        
+        console.log('CFAGKJSLD;gLJGLB');
+        clearInterval(interval);
+        snakeBody[0].classList.add('gameOver');
+        setTimeout(() => {
+        fild.innerHTML = `<p class="gameOverText">Игра ОКОНЧЕНА!!!</p>`;    
+        }, 3000);
+        
+        
+    }
+
+    snakeBody[0].classList.add('snakeHead');
     for (let i = 0; i < snakeBody.length; i++) {
     snakeBody[i].classList.add('snakeBody');
     }
+    addinfBodeSnake()
+    
     ateFood()
+    stop = true;
 }
-
-
 let interval = setInterval(move, 300)
+// move()
+
+window.addEventListener('keydown', function(e) {
+console.log('direction: ', direction);
+    if (stop == true) {
+        if (e.key == "ArrowUp" && (direction != 'ArrowDown')) {
+            // console.log('e.key : ', e.key );
+            direction = 'ArrowUp';
+            stop = false;
+            // console.log('true: ', true);
+        } else if (e.key == "ArrowDown" && (direction != 'ArrowUp')) {
+            // console.log('e.key: ', e.key);
+            
+            // console.log('true: ', true);
+            direction = 'ArrowDown';
+            stop = false;
+        } else if (e.key == "ArrowLeft" && direction != ('ArrowRight')) {
+            // console.log('e.key: ', e.key);
+            
+            // console.log('true: ', true);
+            direction = 'ArrowLeft';
+            stop = false;
+        } else if (e.key == "ArrowRight"  && direction != ('ArrowLeft')) {
+            // console.log('e.key: ', e.key);
+            
+            // console.log('true: ', true);
+            direction = 'ArrowRight';
+            stop = false;
+        }
+    }    
+})
+
 
 function ateFood() {
     const coordinatesSnake = [snakeBody[0].getAttribute('data-X'), snakeBody[0].getAttribute('data-Y')];
     // console.log('coordinatesSnake!!!: ', coordinatesSnake);
-    const food = Array.from(document.getElementsByClassName('food'));
-    console.log('food!!: ', food);
-    const coordinatesFood = [food[0].getAttribute('data-X'), food[0].getAttribute('data-Y')];
-    console.log('coordinatesFood!!!!!!!!!!!!! : ', coordinatesFood );
+    const food = document.querySelector('.food');
+    // console.log('food!!: ', food);
+    const coordinatesFood = [food.getAttribute('data-X'), food.getAttribute('data-Y')];
+    // console.log('coordinatesFood!!!!!!!!!!!!! : ', coordinatesFood );
     // console.log('JSON.stringify(coordinatesFood): ', JSON.stringify(coordinatesFood));
     // console.log('stringify(coordinatesSnake): ', JSON.stringify(coordinatesSnake));
     let newFood = new Coordinate(showRandomSnake(), 'food');
     let coordinat = [ ];
-    if (JSON.stringify(coordinatesSnake) == JSON.stringify(coordinatesFood)) {
+
+    if ((snakeBody[0].getAttribute('data-X') == food.getAttribute('data-X')) && (snakeBody[0].getAttribute('data-Y') == food.getAttribute('data-Y'))) {
         // console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!");
-        food[0].classList.remove('food');
-        
-        newFood.showRadom()
+        food.classList.remove('food');
+       
+        newFood.showRadomFood()
+        // snakeBody[snakeBody.length-1].remove('snakeBody'); 
         addinfBodeSnake()
-        console.log('coordinat%%%%%%%%%%%%%%%%%%%%%%%: ', coordinat);
-        let newCoordinatesFood = [food[0].getAttribute('data-X'), food[0].getAttribute('data-Y')];
-        coordinat.unshift(newCoordinatesFood);  
-        food.splice(2, 4);
+        // console.log('coordinat%%%%%%%%%%%%%%%%%%%%%%%: ', coordinat);
+        // let newCoordinatesFood = [food[0].getAttribute('data-X'), food[0].getAttribute('data-Y')];
+        // coordinat.unshift(newCoordinatesFood);  
+        // food.splice(2, 4);
     } 
 }
 
 // ф-ция сработает, когда змея съест еду
 function addinfBodeSnake() {
-    let newSnakeItemBody = [ ];
-    
-    let addingLeft = [snakeBody[0].getAttribute('data-X'), snakeBody[0].getAttribute('data-Y')];
-    if (direction == "ArrowUp") {
+    const food = document.querySelector('.food');
+    if ((snakeBody[0].getAttribute('data-X') == food.getAttribute('data-X')) && (snakeBody[0].getAttribute('data-Y') == food.getAttribute('data-Y'))) {
+        let a = snakeBody[snakeBody.length - 1].getAttribute('data-X');
+        let b = snakeBody[snakeBody.length - 1].getAttribute('data-Y');
+        snakeBody.push(document.querySelector('[data-X = "' + a + '"][data-Y = "' + b + '"]'));
+        console.log('ПРОВЕРЬ!!!!!!!!: ', snakeBody);
         
-        console.log('addingLeft)))))))))))((((((((((((())))))))))))): ', addingLeft);
-        newSnakeItemBody.push(document.querySelector('[data-X = "' + addingLeft[0] + '"][data-Y = "' + (+addingLeft[1] - 1) + '"]'));
-        newSnakeItemBody[newSnakeItemBody.length - 1].classList.add('snakeBody')
+         
     }
 }
+console.log('ПРОВЕРЬ!!!!!!!!222222: ', snakeBody);
+
